89d73b0f20bb91e4511f935d0ccd937cc77ca65a
sameer-mitra <49610563+sameer-mitra@users.noreply.github.com>
2020-06-16 17:25:07 +1000

    Initial Log and Status Project

---
+++ ProBytes.UI.Status/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ProBytes.UI.Status
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new StatusForm());
+        }
+    }
+}
---
+++ ProBytes.UI.Status/StatusForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProBytes.UI.Status
+{
+    public class StatusForm : System.Windows.Forms.Form  
+    {
+      
+    }
+
+    
+}
