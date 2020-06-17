51f5f5583f323de0b26ef7e9dfa596d027595cd0
sameer-mitra <49610563+sameer-mitra@users.noreply.github.com>
2020-06-17 20:19:07 +1000

    Minor change

--- ProBytes.UI.Status/StatusForm.cs
+++ ProBytes.UI.Status/StatusForm.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace ProBytes.UI.Status
 {
-    public class StatusForm : System.Windows.Forms.Form  
+    public class StatusForm : System.Windows.Forms.Form
     {
-      
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // StatusForm
+            // 
+            this.ClientSize = new System.Drawing.Size(361, 437);
+            this.Name = "StatusForm";
+            this.ResumeLayout(false);
+
+        }
     }
 
     
