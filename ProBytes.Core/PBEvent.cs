9d8603d195d37093173af23c6b34ce17d4135ec1
sameer-mitra <49610563+sameer-mitra@users.noreply.github.com>
2020-06-19 19:55:34 +1000

    SM 20200619 Implemented the EventWriter class

--- ProBytes.Core/PBEvent.cs
+++ ProBytes.Core/PBEvent.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
 
 namespace ProBytes.Core
 {
@@ -36,16 +38,50 @@ namespace ProBytes.Core
             this.LogStageProgress = logStageProgress;
             this.LogTotalProgress = logTotalProgress;
         }
+        public PBEvent(string logCompany, string logActionVerb, string logActionTarget, string logDetails, int logStageProgress, int logTotalProgress)
+        {
+            this.LogTimeSpecified = false;
+            this.LogCompany = logCompany;
+            this.LogActionVerb = logActionVerb;
+            this.LogActionTarget = logActionTarget;
+            this.LogDetails = logDetails.Replace("\r\n", "");
+            this.LogStageProgress = logStageProgress;
+            this.LogTotalProgress = logTotalProgress;
+        }
 
+        public PBEvent(DateTime logTime, string logActionVerb, string logActionTarget, string logDetails)
+        {
+            this.LogTime = logTime;
+            this.LogTimeSpecified = true;
+            this.LogCompany = LogCompany;
+            this.LogActionVerb = logActionVerb;
+            this.LogActionTarget = logActionTarget;
+            this.LogDetails = logDetails.Replace("\r\n", "");
+            this.LogStageProgress = -1;
+            this.LogTotalProgress = 999;
+        }
+
+        public PBEvent(string logCompany, string logActionVerb, string logActionTarget, string logDetails)
+        {
+            this.LogTimeSpecified = false;
+            this.LogCompany = logCompany;
+            this.LogActionVerb = logActionVerb;
+            this.LogActionTarget = logActionTarget;
+            this.LogDetails = logDetails.Replace("\r\n", "");
+            this.LogStageProgress = -1;
+            this.LogTotalProgress = 999;
+        }
 
         public int CompareTo(object obj)
         {
-            PBEvent otherEvent = (PBEvent) obj;
+            PBEvent otherEvent = (PBEvent)obj;
 
-            if (otherEvent == null){
+            if (otherEvent == null)
+            {
                 throw new ArgumentException("Object is not PBEvent.");
             }
-            else{
+            else
+            {
                 if (this.LogTimeSpecified && otherEvent.LogTimeSpecified)
                 {
                     if (this.LogTime.CompareTo(otherEvent.LogTime) == 0)
@@ -137,7 +173,7 @@ namespace ProBytes.Core
             //Details
             LogDetails = LogDetails.Replace("\r", "");
             LogDetails = LogDetails.Replace("\n", "");
-            
+
             if (LogDetails.Length > nLogDetailsLength - 1)
             {
                 LogDetails = LogDetails.Substring(0, nLogDetailsLength - 3) + "...";
@@ -154,7 +190,128 @@ namespace ProBytes.Core
             return sTemp;
 
         }
-    }
 
-    
+        public static PBEvent ParsePBEvent(string sEvent)
+        {
+            int i;
+            int j;
+            var dTime = default(DateTime);
+            string sTemp;
+            string sCompany;
+            string sVerb;
+            string sTarget;
+            string sDetails;
+            int iStageProgress;
+            int iTotalProgress;
+            bool bTimeParsed = false;
+
+            // BM 20140908 F08187 - Handle non english am/pm while using ParseExact method
+            var provider = CultureInfo.InvariantCulture;
+            if (CultureInfo.CurrentCulture.TextInfo.ANSICodePage != 1252)
+            {
+                provider = CultureInfo.CurrentCulture;
+            }
+
+
+            // SM 20100212 F01665 Modified this function to return a default HDM Event when the given string is unable to be parsed.
+            try
+            {
+                if (string.IsNullOrEmpty(sEvent))
+                {
+                    return CreateDefaultPBEvent(DateTime.Now);
+                }
+
+                // Parse the given string to extract variables..
+                // SM 20100212 F01665 Add try and catch blocks to preserve as much information as possible.. 
+                // only set default values for variables which are in error - decided against this approach as..
+                // we would still get incorrect variables due to shifting of the log entry - this could be misleading.
+
+                i = nLogTimeLength - 1;
+                sTemp = sEvent.Substring(0, i);
+                sTemp = sTemp.Trim();
+                // BM 20140908 F08187 - Handle non english am/pm while using ParseExact method
+                // dTime = DateTime.ParseExact(sTemp, "yyyyMMdd hh:mm:ss.ff tt", System.Globalization.CultureInfo.InvariantCulture)
+                try
+                {
+                    dTime = DateTime.ParseExact(sTemp, "yyyyMMdd hh:mm:ss.ff tt", provider);
+                }
+                catch (Exception)
+                {
+                    dTime = DateTime.ParseExact(sTemp, "yyyyMMdd hh:mm:ss.ff tt", CultureInfo.InvariantCulture);
+                }
+
+                bTimeParsed = true;
+                j = i;
+                i = i + nLogCompanyLength;
+                sCompany = sEvent.Substring(j, nLogCompanyLength);
+                sCompany = sCompany.Trim();
+                j = i;
+                i = i + nLogActionVerbLength;
+                sVerb = sEvent.Substring(j, nLogActionVerbLength);
+                sVerb = sVerb.Trim();
+                j = i;
+                i = i + nLogActionTargetLength;
+                sTarget = sEvent.Substring(j, nLogActionTargetLength);
+                sTarget = sTarget.Trim();
+                j = i;
+                i = i + nLogStageProgressLength;
+                sTemp = sEvent.Substring(j, nLogStageProgressLength);
+                sTemp = sTemp.Trim();
+                if (!string.IsNullOrEmpty(sTemp))
+                {
+                    iStageProgress = int.Parse (sTemp);
+                }
+                else
+                {
+                    iStageProgress = -1;
+                }
+
+                j = i;
+                i = i + nLogTotalProgressLength;
+                sTemp = sEvent.Substring(j, nLogTotalProgressLength);
+                sTemp = sTemp.Trim();
+                if (!string.IsNullOrEmpty(sTemp))
+                {
+                    iTotalProgress = int.Parse(sTemp);
+                }
+                else
+                {
+                    iTotalProgress = 999;
+                }
+
+                j = i;
+                sDetails = sEvent.Substring(j);
+                sDetails = sDetails.Trim();
+                sDetails = sDetails.Replace("\r\n", "");
+                var hEvent = new PBEvent(dTime, sCompany, sVerb, sTarget, sDetails, iStageProgress, iTotalProgress);
+                return hEvent;
+            }
+            catch
+            {
+                // SM 20100212 F01665 return a default HDM Event when the given string is unable to be parsed.
+                if (bTimeParsed)
+                {
+                    return CreateDefaultPBEvent(dTime);
+                }
+                else
+                {
+                    return CreateDefaultPBEvent(DateTime.Now);
+                }
+            }
+        }
+
+        private static PBEvent CreateDefaultPBEvent(DateTime dTime)
+        {
+            // SM 20100212 F01665 return a default HDM Event
+            string sCompany = "COMPANY NAME";
+            string sVerb = "ACTION VERB";
+            string sTarget = "ACTION TARGET";
+            string sDetails = "This is a default HDM event, created as an event log entry in the file is not in the specified format and is unable to be parsed. Please check the log file.";
+            int iStageProgress = -1;
+            int iTotalProgress = 999;
+            var hEvent = new PBEvent(dTime, sCompany, sVerb, sTarget, sDetails, iStageProgress, iTotalProgress);
+            return hEvent;
+        }
+
+    }
 }
---
+++ ProBytes.Core/PBEventWriter.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+namespace ProBytes.Core
+{
+    public partial class PBEventWriter
+    {
+        private int iMaxNumberLines;
+        private int iWriteCounter;
+        private int iLatency = 1;
+        private string sFileName;
+        private StreamWriter swWriter;
+        private string[] sCurrentEvents;
+        private const int DEFAULT_MAX_LINES = 200;
+        private const int DEFAULT_LATENCY = 1;
+
+        public PBEventWriter(string fName, int maxLines, int lat)
+        {
+            sFileName = fName;
+            if (maxLines < 1)
+            {
+                iMaxNumberLines = DEFAULT_MAX_LINES;
+            }
+            else
+            {
+                iMaxNumberLines = maxLines;
+            }
+
+            if (lat < 1)
+            {
+                iLatency = DEFAULT_LATENCY;
+            }
+            else
+            {
+                iLatency = lat;
+            }
+
+            CreateEventLog();
+        }
+
+        public PBEventWriter(string fileName, int maxLines)
+        {
+            sFileName = fileName;
+            iLatency = DEFAULT_LATENCY; // Default
+            if (maxLines < 1)
+            {
+                iMaxNumberLines = DEFAULT_MAX_LINES; // Default
+            }
+            else
+            {
+                iMaxNumberLines = maxLines;
+            }
+
+            CreateEventLog();
+        }
+
+        public PBEventWriter(string fileName)
+        {
+            sFileName = fileName;
+            iMaxNumberLines = DEFAULT_MAX_LINES; // Default
+            iLatency = DEFAULT_LATENCY; // Default
+            CreateEventLog();
+        }
+
+        private void CreateEventLog()
+        {
+            try
+            {
+                int i;
+                i = sFileName.LastIndexOf(@"\");
+                string dir = sFileName.Substring(0, i);
+                int nRetry = 0;
+                int nMaxRetries = 10;
+                bool bOK = false;
+                while (nRetry < nMaxRetries)
+                {
+                    try
+                    {
+                        if (!Directory.Exists(dir))
+                        {
+                            Directory.CreateDirectory(dir);
+                        }
+
+                        if (!File.Exists(sFileName))
+                        {
+                            // Create the log file if it does not exist.
+                            swWriter = File.CreateText(sFileName);
+                            swWriter.Close();
+                            Array.Resize(ref sCurrentEvents, 1);
+                        }
+                        else
+                        {
+                            // Return the current Lines.
+                            sCurrentEvents = File.ReadAllLines(sFileName);
+                        }
+
+                        bOK = true;
+                        break;
+                    }
+                    catch
+                    {
+                        nRetry = nRetry + 1;
+                        Thread.Sleep(100);
+                    }
+                }
+
+                if (bOK == false)
+                    return;
+            }
+            catch (Exception ex)
+            {
+                LogError("Create Event Log", ex.ToString());
+            }
+        }
+
+        public void WritePBEvent(PBEvent hEvent, bool bOverWrite = false)
+        {
+            try
+            {
+
+                // SM 20101001 F03324 Add the process ID to the event log entry.
+                hEvent = AddProcessID(hEvent);
+                string newLine = hEvent.ToString();
+                iWriteCounter = iWriteCounter + 1;
+                bool bContinue = true;
+
+                // Prepend the line to the start of the line array.
+                // If overwriting, then replace the last event, no need to resize the array. 
+                int i;
+                if (bOverWrite)
+                {
+                    PBEvent hTemp;
+                    hTemp = PBEvent.ParsePBEvent(sCurrentEvents[0]);
+                    if (hTemp.LogActionVerb.ToUpper() == hEvent.LogActionVerb.ToUpper())
+                    {
+                        sCurrentEvents[0] = newLine;
+                        bContinue = false;
+                    }
+                }
+
+                if (bContinue)
+                {
+                    if (sCurrentEvents.Length - 1 >= iMaxNumberLines - 1)
+                    {
+                        // Shift all array elements by 1.
+                        for (i = sCurrentEvents.Length - 1; i >= 1; i -= 1)
+                            sCurrentEvents[i] = sCurrentEvents[i - 1];
+                        sCurrentEvents[0] = newLine;
+                    }
+                    else
+                    {
+                        // Redim array AND Shift all array elements by 1.
+                        Array.Resize(ref sCurrentEvents, sCurrentEvents.Length + 1 + 1);
+                        for (i = sCurrentEvents.Length - 1; i >= 1; i -= 1)
+                            sCurrentEvents[i] = sCurrentEvents[i - 1];
+                        sCurrentEvents[0] = newLine;
+                    }
+                }
+
+                // Deal with latency..
+                // Only write after a certain number of events.
+                if (iWriteCounter % iLatency == 0)
+                {
+                    int nRetry = 0;
+                    int nMaxRetries = 10;
+                    bool bOK = false;
+                    while (nRetry < nMaxRetries)
+                    {
+                        try
+                        {
+                            swWriter = new StreamWriter(sFileName, false);
+                            using (swWriter)
+                            {
+                                var loopTo = sCurrentEvents.Length-1;
+                                for (i = 0; i <= loopTo; i += 1)
+                                    swWriter.WriteLine(sCurrentEvents[i]);
+                            }
+
+                            // Reset iWriteCounter
+                            iWriteCounter = 0;
+                            bOK = true;
+                            break;
+                        }
+                        catch
+                        {
+                            nRetry = nRetry + 1;
+                            Thread.Sleep(100);
+                        }
+                    }
+
+                    if (bOK == false)
+                        return;
+                }
+            }
+            catch (Exception ex)
+            {
+                LogError("Write HDM Event", ex.ToString());
+            }
+        }
+
+        public void Write()
+        {
+            try
+            {
+                int nRetry = 0;
+                int nMaxRetries = 10;
+                bool bOK = false;
+                while (nRetry < nMaxRetries)
+                {
+                    try
+                    {
+                        swWriter = new StreamWriter(sFileName, false);
+                        int i;
+                        using (swWriter)
+                        {
+                            var loopTo = sCurrentEvents.Length-1;
+                            for (i = 0; i <= loopTo; i += 1)
+                                swWriter.WriteLine(sCurrentEvents[i]);
+                        }
+
+                        // Reset iWriteCounter
+                        iWriteCounter = 0;
+                        bOK = true;
+                        break;
+                    }
+                    catch
+                    {
+                        nRetry = nRetry + 1;
+                        Thread.Sleep(100);
+                    }
+                }
+
+                if (bOK == false)
+                    return;
+            }
+            catch (Exception ex)
+            {
+                LogError("Close/Write", ex.ToString());
+            }
+        }
+
+        public void Close()
+        {
+            Write();
+        }
+
+        private void LogError(string sSource, string sMessage)
+        {
+            try
+            {
+                StreamWriter SW;
+                string errorFile = Assembly.GetExecutingAssembly().Location + @"\PLHDMWRITER.TXT";
+                if (!File.Exists(errorFile))
+                {
+                    SW = File.CreateText(errorFile);
+                }
+                else
+                {
+                    SW = new StreamWriter(errorFile, true);
+                }
+
+                string sLine = "";
+                sLine = sLine + DateTime.Now.ToString( "yyyyMMdd hh:mm:ss tt");
+                sLine = sLine + ": ";
+                sLine = sLine + sSource.PadRight(20, ' ');
+                sLine = sLine + ": ";
+                sLine = sLine + sMessage.Replace("\r\n", " ");
+                using (SW)
+                    SW.WriteLine(sLine);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public void WritePBEvent(DateTime logTime, string logCompany, string logActionVerb, string logActionTarget, string logDetails, int logStageProgress, int logTotalProgress)
+        {
+            PBEvent hEvent;
+            hEvent = new PBEvent(logTime, logCompany, logActionVerb, logActionTarget, logDetails, logStageProgress, logTotalProgress);
+            WritePBEvent(hEvent);
+        }
+
+        public void WritePBEvent(string logCompany, string logActionVerb, string logActionTarget, string logDetails, int logStageProgress, int logTotalProgress)
+        {
+            PBEvent hEvent;
+            hEvent = new PBEvent(logCompany, logActionVerb, logActionTarget, logDetails, logStageProgress, logTotalProgress);
+            WritePBEvent(hEvent);
+        }
+
+        public void WritePBEvent(DateTime logTime, string logActionVerb, string logActionTarget, string logDetails)
+        {
+            PBEvent hEvent;
+            hEvent = new PBEvent(logTime, logActionVerb, logActionTarget, logDetails);
+            WritePBEvent(hEvent);
+        }
+
+        public void WritePBEvent(string logCompany, string logActionVerb, string logActionTarget, string logDetails)
+        {
+            PBEvent hEvent;
+            hEvent = new PBEvent(logCompany, logActionVerb, logActionTarget, logDetails);
+            WritePBEvent(hEvent);
+        }
+
+        public string LogFileName // BP 20100419 F02330 Maintain Log History
+        {
+            get
+            {
+                return sFileName;
+            }
+        }
+
+        // SM 20101001 F03324 Add the process ID to the event log entry.
+        private static PBEvent AddProcessID(PBEvent hEvent)
+        {
+            try
+            {
+                var currentProcess = Process.GetCurrentProcess();
+                string sProcessID = currentProcess.Id.ToString();
+                sProcessID = "[" + sProcessID.PadLeft(6) + "] ";
+
+                // Add the process ID to the log details.
+                hEvent.LogDetails = sProcessID + hEvent.LogDetails;
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+            }
+
+            return hEvent;
+        }
+    }
+}
